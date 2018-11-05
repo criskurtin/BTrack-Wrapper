062c8d853dfb787ed766defe1c5964711b64adba
polardegausser <36190893+polardegausser@users.noreply.github.com>
2018-11-06 00:50:37 +0100

    Add Example Source and Binaries

---
+++ BTrack_Example/BTrack_Example/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BTrack_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDBs = new System.Windows.Forms.Label();
+            this.labelBPM = new System.Windows.Forms.Label();
+            this.AudioDevices = new System.Windows.Forms.ComboBox();
+            this.labelBeatCount = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelCAM = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelDBs
+            // 
+            this.labelDBs.AutoSize = true;
+            this.labelDBs.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelDBs.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelDBs.Location = new System.Drawing.Point(12, 9);
+            this.labelDBs.Name = "labelDBs";
+            this.labelDBs.Size = new System.Drawing.Size(0, 24);
+            this.labelDBs.TabIndex = 0;
+            // 
+            // labelBPM
+            // 
+            this.labelBPM.AutoSize = true;
+            this.labelBPM.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelBPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelBPM.Location = new System.Drawing.Point(239, 57);
+            this.labelBPM.Name = "labelBPM";
+            this.labelBPM.Size = new System.Drawing.Size(0, 24);
+            this.labelBPM.TabIndex = 1;
+            // 
+            // AudioDevices
+            // 
+            this.AudioDevices.FormattingEnabled = true;
+            this.AudioDevices.Location = new System.Drawing.Point(12, 97);
+            this.AudioDevices.Name = "AudioDevices";
+            this.AudioDevices.Size = new System.Drawing.Size(288, 21);
+            this.AudioDevices.TabIndex = 2;
+            this.AudioDevices.SelectedIndexChanged += new System.EventHandler(this.AudioDevices_SelectedDeviceChanged);
+            // 
+            // labelBeatCount
+            // 
+            this.labelBeatCount.AutoSize = true;
+            this.labelBeatCount.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelBeatCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelBeatCount.Location = new System.Drawing.Point(12, 33);
+            this.labelBeatCount.Name = "labelBeatCount";
+            this.labelBeatCount.Size = new System.Drawing.Size(0, 24);
+            this.labelBeatCount.TabIndex = 3;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(142, 128);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(223, 128);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelCAM
+            // 
+            this.labelCAM.AutoSize = true;
+            this.labelCAM.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelCAM.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelCAM.Location = new System.Drawing.Point(12, 57);
+            this.labelCAM.Name = "labelCAM";
+            this.labelCAM.Size = new System.Drawing.Size(81, 24);
+            this.labelCAM.TabIndex = 6;
+            this.labelCAM.Text = "Camera:";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(12, 128);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(92, 23);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.Text = "RESET COUNT";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(310, 163);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelCAM);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelBeatCount);
+            this.Controls.Add(this.AudioDevices);
+            this.Controls.Add(this.labelBPM);
+            this.Controls.Add(this.labelDBs);
+            this.Name = "Form1";
+            this.Text = "Beat Tracking Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDBs;
+        private System.Windows.Forms.Label labelBPM;
+        private System.Windows.Forms.ComboBox AudioDevices;
+        private System.Windows.Forms.Label labelBeatCount;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelCAM;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ BTrack_Example/BTrack_Example/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using NAudio.Wave;
+using NAudio.Dsp;
+using BWrapper;
+using System.Diagnostics;
+
+namespace BTrack_Example
+{
+    public partial class Form1 : Form
+    {
+        ////
+        // WaveIn Objects
+        ////
+        WaveInEvent waveIn = new WaveInEvent();
+        int samplerate = 44100;
+        int channels = 2;
+        private Random rnd = new Random();
+        BTrackWrapper btw = new BTrackWrapper(512, 1024);
+        bool nodevices = false;
+        int beatsdetected = 0;
+        BiQuadFilter lpf = BiQuadFilter.LowPassFilter(44100.0f, 130.0f, 30.0f);
+        int[] beatarray = { 15, 23, 31 };
+
+        public Form1()
+        {
+            InitializeComponent();
+            ////
+            // Initialize Audio
+            ////
+            int devcount = WaveIn.DeviceCount;
+            if (devcount > 0)
+            {
+                for (int i = 0; i < devcount; i++)
+                {
+                    WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(i);
+                    AudioDevices.Items.Add("Device " + i + ": " + deviceInfo.ProductName + ", " + deviceInfo.Channels + " channels");
+                }
+                AudioDevices.SelectedIndex = 0;
+                nodevices = false;
+            }
+            else
+            {
+                nodevices = true;
+            }
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+            buttonReset.Enabled = false;
+
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs args)
+        {
+            double sum = 0;
+            List<double> frame = new List<double>();
+            // interpret as 16 bit audio
+            for (int index = 0; index < 2048; index += 2)
+            {
+                short sample = (short)((args.Buffer[index + 1] << 8) |
+                        args.Buffer[index + 0]);
+                // to floating point
+                var sample32 = sample / 32768.0;
+                sum += (sample32 * sample32);
+                //sample32 = lpf.Transform((float)sample32);
+                //sample32 = lpf.Transform(sample32);
+                if (index % 2 == 0)
+                {
+                    frame.Add(sample32);
+                }
+            }
+
+            var framearray = frame.ToArray();
+            GCHandle handle = GCHandle.Alloc(framearray, GCHandleType.Pinned);
+            var framePtr = handle.AddrOfPinnedObject();
+            btw.processAudioFrameWrapper(framePtr);
+            framearray = null;
+            framePtr = (IntPtr)null;
+            frame.Clear();
+
+            double rms = Math.Sqrt(sum / (2048));
+            var decibel = 20 * Math.Log10(rms);
+            labelDBs.Invoke(new Action(() => { labelDBs.Text = "Power: " + ((int)decibel).ToString() + " dB"; }));
+
+            if (btw.beatDueInCurrentFrameWrapper())
+            {
+                labelBPM.Invoke(new Action(() => { labelBPM.BackColor = Color.Red; }));
+                int beatskip = rnd.Next(1, 3);
+
+                if ((beatsdetected == beatarray[beatskip] || beatsdetected >= 31) && decibel > -60.0) //&& beatsdetected % 2 == 1
+                {
+                    var prevcam = labelBPM.Text;
+                    var nextcam = "";
+                    do
+                    {
+                        nextcam = rnd.Next(1, 4).ToString();
+                    } while (nextcam == prevcam);
+                    labelBPM.Invoke(new Action(() => { labelBPM.Text = nextcam; }));
+                    //labelDBs.Invoke(new Action(() => { labelDBs.Text = btw.getCurrentTempoEstimate().ToString(); }));
+
+                    beatsdetected = 0;
+                }
+                else
+                {
+                    beatsdetected += 1;
+                }
+                labelBeatCount.Invoke(new Action(() => { labelBeatCount.Text = "Beats Detected: " + beatsdetected.ToString(); }));
+            }
+            else
+            {
+                labelBPM.Invoke(new Action(() => { labelBPM.BackColor = Color.Green; }));
+            }
+            handle.Free();
+        }
+
+        private void startAudio()
+        {
+            //run audio for beatmix
+            waveIn.DeviceNumber = AudioDevices.SelectedIndex;
+            waveIn.BufferMilliseconds = 24;
+            waveIn.WaveFormat = new WaveFormat(samplerate, channels);
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.StartRecording();
+            Trace.WriteLine("Recording - Sample rate: " + waveIn.WaveFormat.SampleRate.ToString() + "; Bit depth: " + waveIn.WaveFormat.BitsPerSample.ToString() + "; Channels: " + waveIn.WaveFormat.Channels.ToString());
+        }
+
+        private void stopAudio()
+        {
+            //remove audio for beatmix
+            waveIn.RecordingStopped += WaveIn_RecordingDisconnected;
+            waveIn.StopRecording();
+        }
+
+        private void AudioDevices_SelectedDeviceChanged(object sender, EventArgs e)
+        {
+            if (!nodevices)
+            {
+                waveIn.RecordingStopped += WaveIn_RecordingReset;
+                waveIn.StopRecording();
+            }
+        }
+
+        private void WaveIn_RecordingReset(object sender, StoppedEventArgs e)
+        {
+            waveIn.DataAvailable -= OnDataAvailable;
+            waveIn.Dispose();
+            Trace.WriteLine("Wave In device is now paused.");
+            waveIn.DeviceNumber = AudioDevices.SelectedIndex;
+            waveIn.BufferMilliseconds = 24;
+            waveIn.WaveFormat = new WaveFormat(samplerate, channels);
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.StartRecording();
+            Trace.WriteLine("Recording - Sample rate: " + waveIn.WaveFormat.SampleRate.ToString() + "; Bit depth: " + waveIn.WaveFormat.BitsPerSample.ToString() + "; Channels: " + waveIn.WaveFormat.Channels.ToString());
+            waveIn.RecordingStopped -= WaveIn_RecordingReset;
+        }
+
+        private void WaveIn_RecordingDisconnected(object sender, StoppedEventArgs e)
+        {
+            waveIn.DataAvailable -= OnDataAvailable;
+            waveIn.Dispose();
+            Trace.WriteLine("Wave In device is now halted.");
+            waveIn.RecordingStopped -= WaveIn_RecordingDisconnected;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            startAudio();
+            buttonStart.Enabled = !buttonStart.Enabled;
+            buttonStop.Enabled = !buttonStop.Enabled;
+            buttonReset.Enabled = !buttonReset.Enabled;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            stopAudio();
+            buttonStart.Enabled = !buttonStart.Enabled;
+            buttonStop.Enabled = !buttonStop.Enabled;
+            buttonReset.Enabled = !buttonReset.Enabled;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            beatsdetected = 0;
+        }
+    }
+}
